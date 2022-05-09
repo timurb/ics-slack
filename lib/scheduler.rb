6cb03b809f034a477a45fcccf51b54aa331d5584
Timur Batyrshin <erthad@gmail.com>
2022-05-09 15:19:28 +0300

    fix rubocop warnings, prepare for writing tests

--- lib/scheduler.rb
+++ lib/scheduler.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 #
 # Scheduler runs the specified block every time you invoke scheduler.run
 # but no more often than period passed at init time
@@ -6,40 +8,45 @@
 # and still to be able to control the run frequency
 #
 class Scheduler
-  attr_reader :name
-  attr_reader :period
+  attr_reader :name, :period
 
-  def initialize(name, period=5, logger=Logger.new(STDOUT), &block)
+  def initialize(name, period = 5, logger = Logger.new($stdout), &block)
     @name = name
     @block = block
     @period = normalize_period(period)
     @logger = logger
-    @logger.debug "Next schedule for #{name}: #{next_run}"
+    @logger.debug("Next schedule for #{name}: #{next_run}")
   end
 
-  def run(current_time=Time.now)
-    @logger.debug "Waiting for scheduled time for #{name}: #{next_run}"
-    if next_run <= current_time
-      run!(current_time)
-    end
+  def run
+    @logger.debug("Waiting for scheduled time for #{name}: #{next_run}")
+    return unless next_run <= current_time
   end
 
-  def run!(current_time=Time.now)
-    @logger.info "Running #{name}"
+  def run!
+    @logger.info("Running #{name}")
     @block.call(name)
-    @last_run = Time.now
-    @logger.debug "Next schedule for #{name}: #{next_run}"
+    @last_run = current_time
+    @logger.debug("Next schedule for #{name}: #{next_run}")
   end
 
   def last_run
-    @last_run ||= Time.new(1970)
+    @last_run ||= oldest_time
   end
 
   def next_run
     last_run + period
   end
 
+  def current_time
+    Time.now
+  end
+
   private
+
+  def oldest_time
+    Time.new(1970)
+  end
 
   def normalize_period(period)
     period * 60
